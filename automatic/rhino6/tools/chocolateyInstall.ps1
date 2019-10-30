fab9ad3bbb23e07ae1f010b296ea9e4068f9e788
Florian Frank <f.frank@herzogdemeuron.com>
2019-10-30 14:54:42 +0100

    changed encoding

--- automatic/rhino6/tools/chocolateyInstall.ps1
+++ automatic/rhino6/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://files.mcneel.com/dujour/exe/20191022/rhino_en-us_6.19.19295.01001.exe'
 $checksum   = '99912a024c669fc1d9c6cfb289e5a1469f9a785f20073b5fc0f6314db2924362'
